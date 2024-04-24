647d1600cf0fbb062ff4df40c863a857446e9caa
Trenton Stiles <stiles.trenton@gmail.com>
2024-04-24 14:47:58 -0500

    configuring some progress on LSP and need configs on CMP to help with autocomplete

---
+++ README.md
@@ -0,0 +1,2 @@
+# personal neovim config file for myself
+## do not use, I'm new to nvim and lua :)
--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,10 +1,12 @@
---[[
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({
+        buffer = bufnr,
+        preserve_mappings = false
+    })
 end)
 
 -- here you can setup the language servers
@@ -13,6 +15,21 @@ end)
 -- with the name of a language server you have installed
 -- require('lspconfig').example_server.setup({})
 
+local generic_signature_setup = {
+    bind = true,
+    handler_opts = {
+        border = "rounded"
+    }
+}
+
+local generic_setup = {
+  on_attach = function(client, bufnr)
+    require "lsp_signature".on_attach(generic_signature_setup, bufnr)
+  end,
+}
+
+require('lspconfig').lua_ls.setup(generic_setup)
 require('lspconfig').clangd.setup({})
-require('lspconfig').pyright.setup({})
-]]--
+require('lspconfig').pyright.setup(generic_setup)
+require('lspconfig').cmake.setup({})
+require('lspconfig').rust_analyzer.setup({})
