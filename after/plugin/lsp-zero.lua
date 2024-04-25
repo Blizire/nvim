1c1370f3dba5b84fe6624d59b571a661371314a8
trenton stiles <stiles.trenton@gmail.com>
2024-04-25 17:26:59 -0500

    added vim-illuminate for variable highlighting

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -28,7 +28,27 @@ local generic_setup = {
   end,
 }
 
-require('lspconfig').lua_ls.setup(generic_setup)
+-- must be set so lua_ls will recognize globals for nvim configurations
+-- IMPORTANT: make sure to setup neodev BEFORE lspconfig
+require("neodev").setup({
+  -- add any options here, or leave empty to use the default settings
+})
+
+-- then setup your lsp server as usual
+local lspconfig = require('lspconfig')
+
+-- example to setup lua_ls and enable call snippets
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = "Replace"
+      }
+    }
+  }
+})
+
+--require('lspconfig').lua_ls.setup(generic_setup)
 require('lspconfig').clangd.setup(generic_setup)
 require('lspconfig').pyright.setup(generic_setup)
 require('lspconfig').cmake.setup(generic_setup)
