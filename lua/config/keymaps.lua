67bedecec49d8ea35aad983fe2be87666ba01cc7
pl0xe <pl0xe@mailinator.me>
2024-12-21 19:04:15 -0600

    add keymap for showing full window for inlay linting tips

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,5 @@ vim.keymap.set("n", "<leader>w", ":w<CR>") -- write file
 vim.keymap.set("n", "<leader>q", ":q!<CR>") -- force quit
 vim.keymap.set("n", "<leader>e", ":e .<CR>") -- file explore
 vim.keymap.set("n", "<leader>vv", "<C-v>", { noremap = true }) -- visual block mode
+-- shows full window for lsp inlany hints
+vim.keymap.set("n", "<leader>h", function () vim.diagnostic.open_float() end)
