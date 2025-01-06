94adb7ffa10080674c41f0324f71ec0816dcdc50
pl0xe <pl0xe@mailinator.me>
2025-01-05 21:10:13 -0600

    highlights the line the cursor is on

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,4 @@ vim.opt.scrolloff = 999 -- keeps cursor in middle
 vim.opt.virtualedit = "block" -- true visual block select
 vim.opt.inccommand = "split" -- show replace all changes in new window
 vim.opt.number = true -- have line #s show in the editor
+vim.opt.cursorline = true -- highlight current line being edited / selected
