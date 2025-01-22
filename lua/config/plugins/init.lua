b0494fe2a808d9e2df34c10d6f1ecc3b768b5efb
pl0xe <pl0xe@mailinator.me>
2025-01-22 11:41:05 -0600

    changed theme

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -1,4 +1,3 @@
 require('config.plugins.cmp') -- autocomplete
 require('config.plugins.lsp') -- intellisense
 require('config.plugins.telescope') -- file fuzzy finder
-require('config.plugins.kanagawa') -- set colorscheme theme
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,12 @@
 return {
-	"https://github.com/rebelot/kanagawa.nvim",
+  {
+    "https://github.com/rebelot/kanagawa.nvim",
+  },
+  {
+    "https://github.com/sainnhe/gruvbox-material",
+    init = function (LazyPlugin)
+      vim.o.background = "dark"
+      vim.cmd.colorscheme "gruvbox-material"
+    end
+  }
 }
